18940f9d7f27bd550e87a1991228c98c1d6b7e0f
Casey <casey@xeanto.us>
2023-02-18 00:00:53 -0500

    7910

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './brand.css';
+import {google, slack, atlassian, dropbox, shopify} from './imports.js'
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div className=''><img src={google} alt="google"/></div>
+      <div className=''><img src={slack} alt="slack"/></div>
+      <div className=''><img src={atlassian} alt="atlassian"/></div>
+      <div className=''><img src={dropbox} alt="dropbox"/></div>
+      <div className=''><img src={shopify} alt="shopify"/></div>
+    </div>
   )
 }
 
